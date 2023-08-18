4fd4588a075f8da1b715ab2db9c3b82172bb26b2
AuztinC <40247162+AuztinC@users.noreply.github.com>
2023-08-17 22:23:57 -0400

    Multiletter buttons working. need to finish start function

--- main.js
+++ main.js
@@ -1,50 +1,66 @@
 
-// document.addEventListener("DOMContentLoaded", () =>  { // ON LOAD
-
-
 let bodyParts = [`url("/img/1.jpg")`,`url("/img/2.jpg")`,`url("/img/3.jpg")`,`url("/img/4.jpg")`,`url("/img/5.jpg")`,`url("/img/6.jpg")`,`url("/img/7.jpg")`,`url("/img/8.jpg")`,`url("/img/9.jpg")`,`url("/img/10.jpg")`];
 let f = 0;
 let ranWord = "";
 let wordLength = 4;
 let answerBlock = document.createElement("div")
 let gameBoard = document.getElementById('gameBoard');
+let keyBlock = document.getElementsByClassName('keyBlock');
+let uiBtns = document.getElementsByClassName("btn")
+let keyRow = $('.keyRow');
+let isKey = false;
+let inputBlock = Array.from(document.querySelectorAll('.inputBlock'));
+
+
 // ACCESS RANDOM WORD API
 async function getNewWord(length){
     let something = new XMLHttpRequest();
     something.open("GET", `https://random-word-api.vercel.app/api?words=1&length=${length}`);
-
-
     something.onload = function(){
         ranWord = something.responseText.slice(2, something.response.length - 2);
         // console.log(ranWord);
     }
     something.send();
 }
-// getNewWord(function(w){console.log(w)})
-getNewWord(wordLength)
 
-function setWord(rWord) { // DISPLAY WINNING WORD
-    for (i = 0; i < inputBlock.length; i++) {
-        inputBlock[i].innerHTML = rWord[i].toUpperCase();
-        inputBlock[i].style.visibility='visible'
-    }
+function startGame(){
+
+
+                //  TODOOOOS
+    // reset keyboard
+    // reset images
+    // clear input blocks
+    // append input blocks
+
+
+
+    getNewWord(wordLength)
 }
-// })// END ON LOAD
+startGame()
 
-let inputBlock = Array.from(document.querySelectorAll('.inputBlock'));
-// let container = document.getElementById('container');
-// let keyRow = document.getElementsByClassName('keyRow');
-let keyBlock = document.getElementsByClassName('keyBlock');
-let keyRow = $('.keyRow');
+// console.log(uiBtns[2].className)
+function setWordLength(obj, num){
 
-let isKey = false;
+    for(let i = 0; i < uiBtns.length; i++){
+
+        let btnClass = uiBtns[i].className
+
+        if(btnClass.includes(" selected")){
+            uiBtns[i].className = btnClass.replace(" selected", "");
+            console.log(uiBtns[i])
+        }
+    }
+    obj.className += " selected"
+    wordLength = num;
+    startGame()
+}
+
 function letterCheck(key) { // PRESS ANY KEY
 
-    // console.log(key.offsetTop)
     key.className = "inactiveKey";
     key.disabled = true;
 
-    for (i = 0; i < inputBlock.length; i++) {
+    for (i = 0; i < wordLength; i++) {
         if (key.id[3] == ranWord[i].toUpperCase()) {
             let starBlock = document.createElement('div');
             inputBlock[i].innerHTML = ranWord[i].toUpperCase();
@@ -59,7 +75,7 @@ function letterCheck(key) { // PRESS ANY KEY
         f++;
         gameBoard.style.backgroundImage = bodyParts[f]
     }
-    if (f == wordLength) {
+    if (f == 9) {
         gameOver();
     }
     // console.log(f)
@@ -67,7 +83,14 @@ function letterCheck(key) { // PRESS ANY KEY
 }
 
 function anotherWord(){
-    location.reload();
+    startGame()
+}
+
+function setWord(rWord) { // DISPLAY WINNING WORD
+    for (i = 0; i < inputBlock.length; i++) {
+        inputBlock[i].innerHTML = rWord[i].toUpperCase();
+        inputBlock[i].style.visibility='visible'
+    }
 }
 
 function gameOver() {
